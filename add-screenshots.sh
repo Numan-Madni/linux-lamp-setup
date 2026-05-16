8a8a101b43371ca3e632ee42455baf16b4b2ff39
Numan-Madni <numanmadni1010+linux@gmail.com>
2026-05-17 00:15:29 +0530

    Create add-screenshots.sh

---
+++ add-screenshots.sh
@@ -0,0 +1,14 @@
+#Create folder
+mkdir screenshots
+#Copy images
+cp /path/to/image.png screenshots/
+#Push again
+git add .
+git commit -m "Added screenshots"
+git push
+#Update Existing Project
+git add .
+git commit -m "Updated script / fixed bug"
+git push
+#Check Your GitHub
+https://github.com/your-username
