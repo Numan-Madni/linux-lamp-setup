36650fea75403cc8acde0f438e20c45edee93c7d
Numan-Madni <numanmadni1010+linux@gmail.com>
2026-05-17 00:10:58 +0530

    Create linux-lamp-setup.sh

---
+++ linux-lamp-setup.sh
@@ -0,0 +1,11 @@
+mkdir linux-lamp-setup
+cd linux-lamp-setup
+
+nano install.sh
+nano README.md
+
+git init
+git add .
+git commit -m "Added LAMP setup project"
+git remote add origin https://github.com/your-username/linux-lamp-setup.git
+git push -u origin main
